1c00ddaec67f88cf80a8145005e0e08802d9cbdd
elizabethsotomayor <elizabethrsotomayor@gmail.com>
2021-02-08 20:18:00 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def move_right():
+    tim.right(10)
+    # new_heading = tim.heading() - 10
+    # tim.setheading(new_heading)
+
+
+def move_left():
+    tim.left(10)
+    # new_heading = tim.heading() + 10
+    # tim.heading(new_heading)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="c", fun=clear)
+clear()
+screen.exitonclick()
